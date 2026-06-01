6be7be0467b5963a560c48fc11247ee2ef9d092f
CJDAutorio <cj.dautorio2021@gmail.com>
2026-06-01 17:00:13 -0400

    Task 3 - Validate successful purchase text

--- pages/product.page.ts
+++ pages/product.page.ts
@@ -1,14 +1,73 @@
-import { Page } from "@playwright/test"
+import { expect, Page } from "@playwright/test";
 
 export class Product {
-    private readonly page: Page
-    private readonly addToCart: string = 'button[id="add-to-cart-sauce-labs-backpack"]'
+  private readonly page: Page;
+  private readonly addToCart: string =
+    'button[id="add-to-cart-sauce-labs-backpack"]';
+  private readonly cartButton: string = '[data-test="shopping-cart-link"]';
+  private readonly checkoutButton: string = '[data-test="checkout"]';
+  private readonly checkoutForm = {
+    firstNameInput: '[data-test="firstName"]',
+    lastNameInput: '[data-test="lastName"]',
+    zipCodeInput: '[data-test="postalCode"]',
+  };
+  private readonly continueButton: string = '[data-test="continue"]';
+  private readonly finishButton: string = '[data-test="finish"]';
+  private readonly checkoutCompleteContainer: string =
+    '[data-test="checkout-complete-container"]';
+  private readonly checkoutCompleteHeader: string =
+    '[data-test="complete-header"]';
+  private readonly checkoutCompleteText: string = '[data-test="complete-text"]';
 
-    constructor(page: Page) {
-        this.page = page;
-    }
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-    public async addBackPackToCart() {
-        await this.page.locator(this.addToCart).click()
-    }
+  public async addBackPackToCart() {
+    await this.page.locator(this.addToCart).click();
+  }
+
+  public async clickCartButton() {
+    await this.page.locator(this.cartButton).click();
+  }
+
+  public async clickCheckoutButton() {
+    await this.page.locator(this.checkoutButton).click();
+  }
+
+  public async fillCheckoutForm(
+    firstName: string,
+    lastName: string,
+    zipCode: string,
+  ) {
+    await this.page.locator(this.checkoutForm.firstNameInput).fill(firstName);
+    await this.page.locator(this.checkoutForm.lastNameInput).fill(lastName);
+    await this.page.locator(this.checkoutForm.zipCodeInput).fill(zipCode);
+  }
+
+  public async clickContinueButton() {
+    await this.page.locator(this.continueButton).click();
+  }
+
+  public async clickFinishButton() {
+    await this.page.locator(this.finishButton).click();
+  }
+
+  public async assertSuccessContainerVisible() {
+    await expect(
+      this.page.locator(this.checkoutCompleteContainer),
+    ).toBeVisible();
+  }
+
+  public async assertSuccessHeaderCorrect(expectedText: string) {
+    await expect(
+      this.page.locator(this.checkoutCompleteHeader),
+    ).toHaveText(expectedText);
+  }
+
+  public async assertSuccessTextCorrect(expectedText: string) {
+    await expect(
+      this.page.locator(this.checkoutCompleteText),
+    ).toHaveText(expectedText);
+  }
 }
--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,7 +1,42 @@
-import { Then } from '@cucumber/cucumber';
-import { getPage } from '../playwrightUtilities';
-import { Product } from '../pages/product.page';
+import { Then } from "@cucumber/cucumber";
+import { getPage } from "../playwrightUtilities";
+import { Product } from "../pages/product.page";
 
-Then('I will add the backpack to the cart', async () => {
+Then("I will add the backpack to the cart", async () => {
   await new Product(getPage()).addBackPackToCart();
+});
+
+Then("I will click the cart button", async () => {
+  await new Product(getPage()).clickCartButton();
+});
+
+Then("I will click the checkout button", async () => {
+  await new Product(getPage()).clickCheckoutButton();
+});
+
+Then(
+  "I will fill the checkout form as {string} {string}, {string}",
+  async (firstName, lastName, zipCode) => {
+    await new Product(getPage()).fillCheckoutForm(firstName, lastName, `${zipCode}`);
+  },
+);
+
+Then("I will click the continue button", async () => {
+  await new Product(getPage()).clickContinueButton();
+});
+
+Then("I will click the finish button", async () => {
+  await new Product(getPage()).clickFinishButton();
+});
+
+Then("The checkout success container should be visible", async () => {
+  await new Product(getPage()).assertSuccessContainerVisible();
+});
+
+Then("The checkout success header should read {string}", async (expectedText) => {
+  await new Product(getPage()).assertSuccessHeaderCorrect(expectedText);
+});
+
+Then("The checkout success text should read {string}", async (expectedText) => {
+  await new Product(getPage()).assertSuccessTextCorrect(expectedText);
 });
